f4149eedf1b2a28bec3fc5e28d0546a4964717db
joaoscioli <joaoscioli@outlook.com>
2025-03-17 20:41:32 -0300

    feat(new Project): project 01 from the book 200+ Java Programs for Beginners Hernando Abella example 01 Hello World project

---
+++ src/java_ebook_200_exercices/HelloWord.java
@@ -0,0 +1,16 @@
+package java_ebook_200_exercices;
+
+///
+/// Hello World Project
+///
+/// ## Example
+/// ``` java
+/// Output:
+///     Hello World
+/// ```
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
