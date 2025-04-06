6b9fcf1c3a95b691329404921301c6a0e2b5e657
joaoscioli <joaoscioli@outlook.com>
2025-04-06 18:53:10 -0300

    docs(add documentation): add docs to project 13 CheckPrimeNumber

--- src/java_ebook_200_exercices/CheckPrimeNumber.java
+++ src/java_ebook_200_exercices/CheckPrimeNumber.java
@@ -3,8 +3,30 @@ package java_ebook_200_exercices;
 import java.util.Scanner;
 
 public class CheckPrimeNumber {
+
+    /// # CheckPrimeNumber
+    ///
+    /// This program checks whether a given integer is a **prime number**.
+    ///
+    /// A *prime number* is a number greater than 1 that has no divisors other than 1 and itself.
+    ///
+    /// ## How it works:
+    /// - Prompts the user to enter an integer.
+    /// - Verifies the input is a valid integer greater than 1.
+    /// - Determines if the number is prime using the `isPrime(int)` method.
+    /// - Outputs the result to the console.
+    ///
+    /// ## Example
+    /// ```
+    /// Enter a number: 33
+    /// 33 is not a prime number.
+    /// ```
+    ///
+    /// ## See Also
+    /// - [Prime number - Wikipedia](https://en.wikipedia.org/wiki/Prime_number)
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         // Prompt the user to enter a number
         System.out.print("Enter a number: ");
         if (scanner.hasNextInt()) {
@@ -17,10 +39,18 @@ public class CheckPrimeNumber {
         } else {
             System.out.println("Please enter a valid integer greater than 1.");
         }
+
         scanner.close();
     }
 
-    // Function to check if a number is prime
+    /// ### isPrime
+    ///
+    /// Checks whether a given integer is a **prime number**.
+    ///
+    /// A number is considered prime if it has no divisors other than 1 and itself.
+    ///
+    /// @param number the number to check for primality
+    /// @return `true` if the number is prime; `false` otherwise
     private static boolean isPrime(int number) {
         for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
