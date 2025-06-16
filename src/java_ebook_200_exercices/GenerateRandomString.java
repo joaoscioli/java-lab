1ee8b17e6bf5fa3540923961b7ca52a91ae2d4ae
joaoscioli <joaoscioli@outlook.com>
2025-06-16 18:32:48 -0300

    doc(add documentation): add doc to project 44 GenerateRandomString

--- src/java_ebook_200_exercices/GenerateRandomString.java
+++ src/java_ebook_200_exercices/GenerateRandomString.java
@@ -1,22 +1,51 @@
-
 package java_ebook_200_exercices;
 
 import java.security.SecureRandom;
 
+/// # GenerateRandomString
+///
+/// This class provides functionality to generate a random alphanumeric string.
+/// It uses a secure random number generator to ensure high-quality randomness.
+///
+/// ## Example usage:
+/// ```java
+/// String randomStr = GenerateRandomString.generateRandomString(8);
+/// System.out.println("Random String: " + randomStr);
+/// ```
+///
+/// ## Output example:
+/// ```text
+/// Random String: jQmNYWWC
+/// ```
 public class GenerateRandomString {
+
+    /// The set of characters used to generate the random string.
     private static final String ALPHANUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    /// The default length for generated random strings.
     private static final int RANDOM_STRING_LENGTH = 8;
+
+    /// A secure random number generator for producing cryptographically strong random values.
     private static final SecureRandom random = new SecureRandom();
 
+    /// The main method to demonstrate the random string generation.
+    ///
+    /// It generates a random string of length `RANDOM_STRING_LENGTH`
+    /// and prints it to the console.
     public static void main(String[] args) {
-        // Generate a random string of length 8
         String randomString = generateRandomString(RANDOM_STRING_LENGTH);
-
-        // Display the random string
         System.out.println("Random String: " + randomString);
     }
 
-    // Function to generate a random string of a given length
+    /// Generates a random alphanumeric string of the specified length.
+    ///
+    /// @param length the desired length of the random string
+    /// @return a randomly generated string containing letters and digits
+    ///
+    /// ## Example:
+    /// ```java
+    /// String str = GenerateRandomString.generateRandomString(10);
+    /// ```
     public static String generateRandomString(int length) {
         StringBuilder sb = new StringBuilder(length);
 
@@ -24,10 +53,7 @@ public class GenerateRandomString {
             int index = random.nextInt(ALPHANUMERIC.length());
             sb.append(ALPHANUMERIC.charAt(index));
         }
+
         return sb.toString();
     }
 }
-
-/*
-Random String: jQmNYWWC
-*/
