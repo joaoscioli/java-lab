b2d04247c943925e92fd0b5582cfd59d72e8fd18
joaoscioli <joaoscioli@outlook.com>
2025-07-08 20:45:31 -0300

    doc(add documentation): add doc to project 54 DisplayCurrentDate

--- src/java_ebook_200_exercices/DisplayCurrentDate.java
+++ src/java_ebook_200_exercices/DisplayCurrentDate.java
@@ -3,6 +3,23 @@ package java_ebook_200_exercices;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/// # DisplayCurrentDate
+///
+/// This class demonstrates how to obtain and format the current date in Java.
+///
+/// ## Features
+/// - Uses `LocalDate.now()` to get the current system date.
+/// - Formats the date into the `MM/dd/yyyy` format using `DateTimeFormatter`.
+/// - Outputs the formatted date to the console.
+///
+/// ## Example Output
+/// ```
+/// Current Date: 09/20/2024
+/// ```
+///
+/// ## See Also
+/// - [`LocalDate`](https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/time/LocalDate.html)
+/// - [`DateTimeFormatter`](https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/time/format/DateTimeFormatter.html)
 public class DisplayCurrentDate {
     public static void main(String[] args) {
 
@@ -17,7 +34,3 @@ public class DisplayCurrentDate {
         System.out.println("Current Date: " + formattedDate);
     }
 }
-
-/*
-Current Date: 09/20/2024
-*/
