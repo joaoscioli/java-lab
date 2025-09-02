f4d8e9bb4cfe237ca6510eeca0990236c57aa9e0
joaoscioli <joaoscioli@outlook.com>
2025-09-02 19:16:29 -0300

    feat(new project) add new project 79 ConsoleOutput

---
+++ src/java_ebook_200_exercices/ConsoleOutput.java
@@ -0,0 +1,22 @@
+package java_ebook_200_exercices;
+
+public class ConsoleOutput {
+    public static void main(String[] args) {
+        // Write a message to the console
+        System.out.println("Hello, world!");
+
+        // You can also print variables or expressions
+        int number = 42;
+        System.out.println("The answer is: " + number);
+
+        // Multiple values can be printed in a single statement
+        String firstName = "John";
+        String lastName = "Doe";
+        System.out.println("Full Name: " + firstName + " " + lastName);
+    }
+}
+
+// Output:
+// Hello, world!
+// The answer is: 42
+// Full Name: John Doe
