37c8478fd7a32285dc9d4e30e1dd0ca5aac866d9
joaoscioli <joaoscioli@outlook.com>
2025-09-04 19:15:34 -0300

    feat(new project) add new project 80 DateToNumber

---
+++ src/java_ebook_200_exercices/DateToNumber.java
@@ -0,0 +1,15 @@
+package java_ebook_200_exercices;
+
+public class DateToNumber {
+    public static void main(String[] args) {
+
+        // Get the current time in milliseconds since Unix Epoch
+        long millisecondsSinceEpoch = System.currentTimeMillis();
+
+        // Print the numerical representation in milliseconds
+        System.out.println("Current Time in milliseconds since Unix Epoch: " + millisecondsSinceEpoch);
+    }
+}
+
+// Output:
+// Current Time in milliseconds since Unix Epoch: 1726202472472
