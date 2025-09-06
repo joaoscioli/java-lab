dfa2c93f56866cd150e43107775731ca9cd20a8d
joaoscioli <joaoscioli@outlook.com>
2025-09-05 21:48:43 -0300

    docs(add documentation) add detailed documentation to project 80 DateToNumber

--- src/java_ebook_200_exercices/DateToNumber.java
+++ src/java_ebook_200_exercices/DateToNumber.java
@@ -1,5 +1,27 @@
 package java_ebook_200_exercices;
 
+/// # DateToNumber
+///
+/// This class demonstrates how to retrieve the current system time in **milliseconds**
+/// since the Unix Epoch (January 1, 1970, 00:00:00 GMT).
+///
+/// The program uses `System.currentTimeMillis()` to obtain the numerical representation
+/// of the current date and time as a `long` value.
+///
+/// ## Example
+/// ```java
+/// long millisecondsSinceEpoch = System.currentTimeMillis();
+/// System.out.println("Current Time in milliseconds since Unix Epoch: " + millisecondsSinceEpoch);
+/// ```
+///
+/// ## Output
+/// ```text
+/// Current Time in milliseconds since Unix Epoch: 1726202472472
+/// ```
+///
+/// ## Notes
+/// - The returned value is useful for time calculations, measuring elapsed time, or creating unique timestamps.
+/// - The value will change every time the program is executed.
 public class DateToNumber {
     public static void main(String[] args) {
 
@@ -10,6 +32,3 @@ public class DateToNumber {
         System.out.println("Current Time in milliseconds since Unix Epoch: " + millisecondsSinceEpoch);
     }
 }
-
-// Output:
-// Current Time in milliseconds since Unix Epoch: 1726202472472
