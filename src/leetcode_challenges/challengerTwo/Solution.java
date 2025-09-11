75f7f040384c85cab76642c7a7a4b85cf42d79cb
joaoscioli <joaoscioli@outlook.com>
2025-09-11 18:27:09 -0300

    feat(new project) add new project ChallengerTwo file LeetCode Challenger

---
+++ src/leetcode_challenges/challengerTwo/Solution.java
@@ -0,0 +1,14 @@
+package leetcode_challenges.challengerTwo;
+
+public class Solution {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode nodeOne = new ListNode(l1.val);
+        ListNode nodeTwo = new ListNode(l2.val);
+
+        return nodeOne;
+    }
+
+    static void main() {
+        System.out.println(addTwoNumbers(, null));
+    }
+}
