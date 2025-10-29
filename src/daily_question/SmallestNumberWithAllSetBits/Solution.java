56d2173676a048ab4f818bc592da22c11e59facc
joaoscioli <joaoscioli@outlook.com>
2025-10-29 19:16:14 -0300

    feat(daily question) add new project SmallestNumberWithAllSetBits from leetcode

---
+++ src/daily_question/SmallestNumberWithAllSetBits/Solution.java
@@ -0,0 +1,11 @@
+package daily_question.SmallestNumberWithAllSetBits;
+
+public class Solution {
+    public int smallestNumber(int n) {
+        int k = 1;
+        while (((1 << k) - 1) < n) {
+            k++;
+        }
+        return (1 << k) - 1;
+    }
+}
