f7e57ca576eb98c11d9d039b3b8c0a2308913bc2
joaoscioli <joaoscioli@outlook.com>
2025-12-07 21:28:34 -0300

    feat(java book) add new project CheckStringAnagramAnotherString

---
+++ src/java_ebook_200_exercices/CheckStringAnagramAnotherString.java
@@ -0,0 +1,39 @@
+package java_ebook_200_exercices;
+
+import java.util.HashMap;
+
+public class CheckStringAnagramAnotherString {
+    public static boolean areAnagrams(String str1, String str2) {
+    // Function to clean and count characters
+        HashMap<Character, Integer> cleanAndCount(String s) {
+            HashMap<Character, Integer> count = new HashMap<>();
+            for (char c : s.toCharArray()) {
+                if (Character.isAlphabetic(c)) {
+                    char lowerChar = Character.toLowerCase(c);
+                    count.put(lowerChar, count.getOrDefault(lowerChar, 0) + 1);
+                }
+            }
+            return count;
+        }
+
+        // Get character counts for both strings
+        HashMap<Character, Integer> count1 = cleanAndCount(str1);
+        HashMap<Character, Integer> count2 = cleanAndCount(str2);
+
+        // Compare the two frequency counts
+        return count1.equals(count2);
+
+    }
+
+    public static void main(String[] args) {
+
+        String string1 = "listen";
+        String string2 = "silent";
+
+        boolean result = areAnagrams(string1, string2);
+        System.out.println(string1 + " and " + string2 + " are anagrams: " + result);
+    }
+}
+
+// Output:
+// listen and silent are anagrams: true
