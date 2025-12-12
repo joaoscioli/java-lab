568417edfd2d3b03180c070fa36d027ffa069855
joaoscioli <joaoscioli@outlook.com>
2025-12-11 21:27:05 -0300

    feat(leetcode challenger) add solution project challengerTwelfth

--- src/leetcode_challenges/challengerTwelfth/Solution.java
+++ src/leetcode_challenges/challengerTwelfth/Solution.java
@@ -1,5 +1,31 @@
 package leetcode_challenges.challengerTwelfth;
 
 public class Solution {
+    public String intToRoman(int num) {
+        if (num <= 0 || num > 3999) {
+            throw new IllegalArgumentException("Invalid input. Please provide a valid positive integer within the range 1 to 3999.");
+        }
 
+        String[] romanSymbols = {
+                "M", "CM", "D", "CD",
+                "C", "XC", "L", "XL",
+                "X", "IX", "V", "IV", "I"
+        };
+
+        int[] values = {
+                1000, 900, 500, 400,
+                100, 90, 50, 40,
+                10, 9, 5, 4, 1
+        };
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                result.append(romanSymbols[i]);
+                num -= values[i];
+            }
+        }
+        return result.toString();
+    }
 }
