d2be060a2f7976961ccb1cbab7fc4b8855926436
joaoscioli <joaoscioli@outlook.com>
2025-12-13 20:37:14 -0300

    feat(leetcode challenger) add solution to project challengerThirteen

--- src/leetcode_challenges/challengerThirteen/Solution.java
+++ src/leetcode_challenges/challengerThirteen/Solution.java
@@ -1,6 +1,31 @@
 package leetcode_challenges.challengerThirteen;
 
 
-public class Solution {
+import java.util.Map;
 
+public class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> values = Map.of(
+                'I', 1,
+                'V', 5,
+                'X', 10,
+                'L', 50,
+                'C', 100,
+                'D', 500,
+                'M', 1000
+        );
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = values.get(s.charAt(i));
+            if (i + 1 < s.length() && current < values.get(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        }
+
+        return result;
+    }
 }
