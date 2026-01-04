a1f132985bf8ed4ff87093e8140d2c6f4ca04ff6
joaoscioli <joaoscioli@outlook.com>
2026-01-04 19:55:04 -0300

    feat(leetcode challenger) add new project challengerFifteen

---
+++ src/leetcode_challenges/challengerFifteen/Solution.java
@@ -0,0 +1,47 @@
+package leetcode_challenges.challengerFifteen;
+
+import java.io.IO;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1, right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }
+                else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return res;
+    }
+
+
+    void main() {
+        int[] nums = {-1,0,1,2,-1,-4};
+        IO.println(threeSum(nums));
+    }
+}
