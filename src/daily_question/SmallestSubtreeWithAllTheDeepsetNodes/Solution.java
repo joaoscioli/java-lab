e6fe21273e3c1d50f3cf0c7dff24ed268ba15472
joaoscioli <joaoscioli@outlook.com>
2026-01-08 22:12:41 -0300

    feat(leetcode challenger) add new project SmallestSubtreeWithAllTheDeepsetNodes

---
+++ src/daily_question/SmallestSubtreeWithAllTheDeepsetNodes/Solution.java
@@ -0,0 +1,50 @@
+package daily_question.SmallestSubtreeWithAllTheDeepsetNodes;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private static class Result {
+        TreeNode node;
+        int depth;
+
+        Result(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    private Result dfs(TreeNode root) {
+        if (root == null) {
+            return new Result(null, 0);
+        }
+
+        Result left = dfs(root.left);
+        Result right = dfs(root.right);
+
+        if (left.depth > right.depth) {
+            return new Result(left.node, left.depth + 1);
+        } else if (right.depth > left.depth) {
+            return new Result(right.node, right.depth + 1);
+        } else {
+            // depths iguais → este nó é o menor subtree
+            return new Result(root, left.depth + 1);
+        }
+    }
+}
