8e3cdbfd149ee10e032a615002c653c0b776a5d0
joaoscioli <joaoscioli@outlook.com>
2026-01-19 15:38:46 -0300

    feat(leetcode beginners guide) add new project ransomNote

---
+++ src/leetcode_beginners_guide/ransomNote/Solution.java
@@ -0,0 +1,19 @@
+package leetcode_beginners_guide.ransomNote;
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        for (char c : magazine.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (--count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
