cf4a97cb8594c1b757ed279eeaaefdd8fc1875ee
joaoscioli <joaoscioli@outlook.com>
2026-01-21 21:27:06 -0300

    feat(daily question) add new project constructMinimumBitwiseArrayII

---
+++ src/daily_question/constructMinimumBitwiseArrayII/Solution.java
@@ -0,0 +1,29 @@
+package daily_question.constructMinimumBitwiseArrayII;
+
+import java.util.List;
+
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int p = nums.get(i);
+
+            if ((p & 1) == 0) {
+                ans[i] = -1;
+                continue;
+            }
+
+            int k = 0;
+            int temp = p;
+            while ((temp & 1) == 1) {
+                k++;
+                temp >>= 1;
+            }
+
+            ans[i] = p - (1 << (k - 1));
+        }
+        return ans;
+    }
+}
