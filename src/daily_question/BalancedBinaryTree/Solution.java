c73f6effd87043c0147d15bb5fd312f836be8b0d
joaoscioli <joaoscioli@outlook.com>
2026-02-07 22:01:03 -0300

    feat(daily_question) add solution project BalancedBinaryTree

--- src/daily_question/BalancedBinaryTree/Solution.java
+++ src/daily_question/BalancedBinaryTree/Solution.java
@@ -1,4 +1,40 @@
 package daily_question.BalancedBinaryTree;
 
 public class Solution {
+
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = height(node.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+
+        int rightHeight = height(node.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
 }
