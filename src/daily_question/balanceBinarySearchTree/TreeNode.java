53bc87c2a64cffe6484af016d5ad76bd31097117
joaoscioli <joaoscioli@outlook.com>
2026-02-08 21:19:19 -0300

    feat(daily question) add new project balanceBinarySearchTree

---
+++ src/daily_question/balanceBinarySearchTree/TreeNode.java
@@ -0,0 +1,19 @@
+package daily_question.balanceBinarySearchTree;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
