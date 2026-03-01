51e35f62d02fbc859832d40599730b47775ba807
joaoscioli <joaoscioli@outlook.com>
2026-02-28 21:04:34 -0300

    feat(daily question) add new project minimumOperationsEqualizeBinaryString

---
+++ src/daily_question/PartitioningIntoMinimumNumberOfDeciBinaryNumbers/Solution.java
@@ -0,0 +1,13 @@
+package daily_question.PartitioningIntoMinimumNumberOfDeciBinaryNumbers;
+
+class Solution {
+    public int minPartitions(String n) {
+        int maxDigit = 0;
+
+        for (char c : n.toCharArray()) {
+            maxDigit = Math.max(maxDigit, c - '0');
+        }
+
+        return maxDigit;
+    }
+}
