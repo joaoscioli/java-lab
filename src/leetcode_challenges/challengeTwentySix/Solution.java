3885b07d6ebdadcddb62aeff16bee5c882335b72
joaoscioli <joaoscioli@outlook.com>
2026-03-02 20:53:37 -0300

    feat(leetcode challenger) add Solution to project challengeTwentySix

--- src/leetcode_challenges/challengeTwentySix/Solution.java
+++ src/leetcode_challenges/challengeTwentySix/Solution.java
@@ -1,4 +1,19 @@
 package leetcode_challenges.challengeTwentySix;
 
-public class Solution {
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int i = 0;
+
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
+            }
+        }
+
+        return n;
+    }
 }
