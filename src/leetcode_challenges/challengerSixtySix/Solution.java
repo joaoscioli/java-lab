bdcf48dafc87a8ed4948b02b084f7d613e7545b4
joaoscioli <joaoscioli@outlook.com>
2026-03-02 23:34:42 -0300

    feat(leetcode challenger) add Solution to project challengerSixtySix

--- src/leetcode_challenges/challengerSixtySix/Solution.java
+++ src/leetcode_challenges/challengerSixtySix/Solution.java
@@ -1,4 +1,18 @@
 package leetcode_challenges.challengerSixtySix;
 
-public class Solution {
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
 }
