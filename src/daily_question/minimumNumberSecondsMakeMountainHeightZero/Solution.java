ca7de3e0b1a39de04703e7b504f50cb403b8c673
joaoscioli <joaoscioli@outlook.com>
2026-03-12 21:37:40 -0300

    feat(daily question) add complete solution to project minimumNumberSecondsMakeMountainHeightZero

--- src/daily_question/minimumNumberSecondsMakeMountainHeightZero/Solution.java
+++ src/daily_question/minimumNumberSecondsMakeMountainHeightZero/Solution.java
@@ -1,4 +1,37 @@
 package daily_question.minimumNumberSecondsMakeMountainHeightZero;
 
-public class Solution {
+class Solution {
+    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+        long left = 0;
+        long right = (long) 1e18;
+        long ans = right;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+
+            if (can(mid, mountainHeight, workerTimes)) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean can(long time, int mountainHeight, int[] workerTimes) {
+        long total = 0;
+
+        for (int w : workerTimes) {
+
+            long val = (long)(Math.sqrt(1 + (8.0 * time) / w) - 1) / 2;
+
+            total += val;
+
+            if (total >= mountainHeight) return true;
+        }
+
+        return total >= mountainHeight;
+    }
 }
