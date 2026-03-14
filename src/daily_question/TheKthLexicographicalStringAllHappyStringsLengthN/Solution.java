037b59a48d162dbc910e56dc046219acebdb01f0
joaoscioli <joaoscioli@outlook.com>
2026-03-13 22:02:22 -0300

    feat(daily question) add solution to project TheKthLexicographicalStringAllHappyStringsLengthN

--- src/daily_question/TheKthLexicographicalStringAllHappyStringsLengthN/Solution.java
+++ src/daily_question/TheKthLexicographicalStringAllHappyStringsLengthN/Solution.java
@@ -1,4 +1,36 @@
 package daily_question.TheKthLexicographicalStringAllHappyStringsLengthN;
 
-public class Solution {
+class Solution {
+    private int count = 0;
+    private String result = "";
+
+    public String getHappyString(int n, int k) {
+        backtrack(new StringBuilder(), n, k);
+        return result;
+    }
+
+    private void backtrack(StringBuilder current, int n, int k) {
+        if (current.length() == n) {
+            count++;
+            if (count == k) {
+                result = current.toString();
+            }
+            return;
+        }
+
+        for (char c : new char[]{'a', 'b', 'c'}) {
+
+            if (current.length() > 0 &&
+                    current.charAt(current.length() - 1) == c) {
+                continue;
+            }
+
+            current.append(c);
+            backtrack(current, n, k);
+
+            if (!result.isEmpty()) return;
+
+            current.deleteCharAt(current.length() - 1);
+        }
+    }
 }
