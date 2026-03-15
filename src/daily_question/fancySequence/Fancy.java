0f7965c92ba44522c8fbc8eb2836e09fd58a8b19
joaoscioli <joaoscioli@outlook.com>
2026-03-14 21:08:30 -0300

    feat(daily question) add part 01 class Fancy to project fancySequency

--- src/daily_question/fancySequence/Fancy.java
+++ src/daily_question/fancySequence/Fancy.java
@@ -1,4 +1,33 @@
 package daily_question.fancySequence;
 
-public class Fancy {
+class Fancy {
+
+    public Fancy() {
+
+    }
+
+    public void append(int val) {
+
+    }
+
+    public void addAll(int inc) {
+
+    }
+
+    public void multAll(int m) {
+
+    }
+
+    public int getIndex(int idx) {
+
+    }
 }
+
+/**
+ * Your Fancy object will be instantiated and called as such:
+ * Fancy obj = new Fancy();
+ * obj.append(val);
+ * obj.addAll(inc);
+ * obj.multAll(m);
+ * int param_4 = obj.getIndex(idx);
+ */
