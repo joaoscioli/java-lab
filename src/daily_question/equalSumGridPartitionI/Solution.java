28bd11a45f98d66ffa3539e441cd359f5ead26fe
joaoscioli <joaoscioli@outlook.com>
2026-03-24 23:04:17 -0300

    feat(daily question) complete solution to project equalSumGridPartitionI

--- src/daily_question/equalSumGridPartitionI/Solution.java
+++ src/daily_question/equalSumGridPartitionI/Solution.java
@@ -1,4 +1,40 @@
 package daily_question.equalSumGridPartitionI;
 
-public class Solution {
+class Solution {
+    public boolean canPartitionGrid(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        long total = 0;
+        long[] colSum = new long[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                total += grid[i][j];
+                colSum[j] += grid[i][j];
+            }
+        }
+
+        if (total % 2 != 0) return false;
+
+        long target = total / 2;
+
+        long rowPrefix = 0;
+        for (int i = 0; i < m - 1; i++) {
+            long rowSum = 0;
+            for (int j = 0; j < n; j++) {
+                rowSum += grid[i][j];
+            }
+            rowPrefix += rowSum;
+
+            if (rowPrefix == target) return true;
+        }
+
+        long colPrefix = 0;
+        for (int j = 0; j < n - 1; j++) {
+            colPrefix += colSum[j];
+            if (colPrefix == target) return true;
+        }
+
+        return false;
+    }
 }
