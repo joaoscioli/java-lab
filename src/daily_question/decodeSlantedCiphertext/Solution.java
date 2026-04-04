5063506f14e9aed1ad3b241cf1b73723e9175eb0
joaoscioli <joaoscioli@outlook.com>
2026-04-03 22:40:59 -0300

    feat(daily question) complete solution to project decodeSlantedCiphertext

--- src/daily_question/decodeSlantedCiphertext/Solution.java
+++ src/daily_question/decodeSlantedCiphertext/Solution.java
@@ -1,4 +1,38 @@
 package daily_question.decodeSlantedCiphertext;
 
-public class Solution {
+class Solution {
+    public String decodeCiphertext(String encodedText, int rows) {
+        if (rows == 1) return encodedText;
+
+        int n = encodedText.length();
+        int cols = n / rows;
+
+        char[][] matrix = new char[rows][cols];
+
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = encodedText.charAt(index++);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int startCol = 0; startCol < cols; startCol++) {
+            int i = 0, j = startCol;
+
+            while (i < rows && j < cols) {
+                result.append(matrix[i][j]);
+                i++;
+                j++;
+            }
+        }
+
+        int end = result.length() - 1;
+        while (end >= 0 && result.charAt(end) == ' ') {
+            end--;
+        }
+
+        return result.substring(0, end + 1);
+    }
 }
