b8ae91733406fbb15fa3dff9f0a25b7ba8521f61
joaoscioli <joaoscioli@outlook.com>
2026-04-05 21:31:49 -0300

    feat(daily question) complete class to project walkingRobotSimulation

--- src/daily_question/walkingRobotSimulation/Solution.java
+++ src/daily_question/walkingRobotSimulation/Solution.java
@@ -1,7 +1,48 @@
 package daily_question.walkingRobotSimulation;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
+        Set<String> obs = new HashSet<>();
+        for (int[] o : obstacles) {
+            obs.add(o[0] + "," + o[1]);
+        }
 
+        int[][] dirs = {
+                {0, 1},   // North
+                {1, 0},   // East
+                {0, -1},  // South
+                {-1, 0}   // West
+        };
+
+        int x = 0, y = 0;
+        int dir = 0;
+        int maxDist = 0;
+
+        for (int cmd : commands) {
+            if (cmd == -1) {
+                dir = (dir + 1) % 4; // turn right
+            } else if (cmd == -2) {
+                dir = (dir + 3) % 4; // turn left
+            } else {
+                for (int i = 0; i < cmd; i++) {
+                    int nx = x + dirs[dir][0];
+                    int ny = y + dirs[dir][1];
+
+                    if (obs.contains(nx + "," + ny)) {
+                        break;
+                    }
+
+                    x = nx;
+                    y = ny;
+
+                    maxDist = Math.max(maxDist, x * x + y * y);
+                }
+            }
+        }
+
+        return maxDist;
     }
 }
