d20444bdfa5c1202df10c6865d81228aa6d1aed6
joaoscioli <joaoscioli@outlook.com>
2026-04-10 21:10:14 -0300

    feat(daily question) complete Solution class to project MinimumDistanceBetweenThreeEqualElementsII

--- src/daily_question/minimumDistanceBetweenThreeEqualElementsII/Solution.java
+++ src/daily_question/minimumDistanceBetweenThreeEqualElementsII/Solution.java
@@ -1,4 +1,27 @@
 package daily_question.minimumDistanceBetweenThreeEqualElementsII;
 
-public class Solution {
+class Solution {
+    public int minimumDistance(int[] nums) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+
+        int minDistance = Integer.MAX_VALUE;
+
+        for (List<Integer> indices : map.values()) {
+            if (indices.size() < 3) continue;
+
+            for (int i = 0; i <= indices.size() - 3; i++) {
+                int left = indices.get(i);
+                int right = indices.get(i + 2);
+
+                int distance = 2 * (right - left);
+                minDistance = Math.min(minDistance, distance);
+            }
+        }
+
+        return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
+    }
 }
