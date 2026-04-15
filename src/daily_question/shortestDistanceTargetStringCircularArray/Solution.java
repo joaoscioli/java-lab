ad13bd3e6499adeea7399673186217d363d784b0
joaoscioli <joaoscioli@outlook.com>
2026-04-14 21:49:48 -0300

    feat(daily question) complete solution to project ShortestDistanceTargetStringCircularArray

--- src/daily_question/shortestDistanceTargetStringCircularArray/Solution.java
+++ src/daily_question/shortestDistanceTargetStringCircularArray/Solution.java
@@ -1,4 +1,20 @@
 package daily_question.shortestDistanceTargetStringCircularArray;
 
-public class Solution {
+class Solution {
+    public int closestTarget(String[] words, String target, int startIndex) {
+        int n = words.length;
+        int minDistance = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (words[i].equals(target)) {
+                int forward = (i - startIndex + n) % n;
+                int backward = (startIndex - i + n) % n;
+
+                int distance = Math.min(forward, backward);
+                minDistance = Math.min(minDistance, distance);
+            }
+        }
+
+        return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
+    }
 }
