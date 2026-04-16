549fc3aa1424d39c37c58e28d88e49d0d6b65ef0
joaoscioli <joaoscioli@outlook.com>
2026-04-15 21:32:14 -0300

    feat(daily question) complete solution to project ClosestEqualElementQueries

--- src/daily_question/closestEqualElementQueries/Solution.java
+++ src/daily_question/closestEqualElementQueries/Solution.java
@@ -1,4 +1,38 @@
 package daily_question.closestEqualElementQueries;
 
-public class Solution {
+import java.util.*;
+
+class Solution {
+    public List<Integer> solveQueries(int[] nums, int[] queries) {
+        int n = nums.length;
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int idx : queries) {
+            int val = nums[idx];
+            List<Integer> list = map.get(val);
+
+            if (list.size() == 1) {
+                result.add(-1);
+                continue;
+            }
+
+            int pos = Collections.binarySearch(list, idx);
+
+            int prev = list.get((pos - 1 + list.size()) % list.size());
+            int next = list.get((pos + 1) % list.size());
+
+            int dist1 = Math.min(Math.abs(idx - prev), n - Math.abs(idx - prev));
+            int dist2 = Math.min(Math.abs(idx - next), n - Math.abs(idx - next));
+
+            result.add(Math.min(dist1, dist2));
+        }
+
+        return result;
+    }
 }
