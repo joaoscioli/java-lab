55d7781edbc07179eaa6ddd230486d3cb37d4c48
joaoscioli <joaoscioli@outlook.com>
2026-04-17 23:15:39 -0300

    feat(daily question) complete solution to project mirrorDistanceOfAnInteger

--- src/daily_question/mirrorDistanceOfAnInteger/Solution.java
+++ src/daily_question/mirrorDistanceOfAnInteger/Solution.java
@@ -1,4 +1,16 @@
 package daily_question.mirrorDistanceOfAnInteger;
 
-public class Solution {
+class Solution {
+    public int mirrorDistance(int n) {
+        int reversed = reverse(n);
+        return Math.abs(n - reversed);
+    }
+    private int reverse(int x) {
+        int rev = 0;
+        while (x > 0) {
+            rev = rev * 10 + (x % 10);
+            x /= 10;
+        }
+        return rev;
+    }
 }
