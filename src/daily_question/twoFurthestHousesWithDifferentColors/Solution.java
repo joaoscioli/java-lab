1bd330dccdd974771fac1662c2f4fc560835262b
joaoscioli <joaoscioli@outlook.com>
2026-04-19 22:18:52 -0300

    feat(daily question) complete Solution class to project TwoFurthestHousesWithDifferentColors

--- src/daily_question/twoFurthestHousesWithDifferentColors/Solution.java
+++ src/daily_question/twoFurthestHousesWithDifferentColors/Solution.java
@@ -1,4 +1,27 @@
 package daily_question.twoFurthestHousesWithDifferentColors;
 
 public class Solution {
+    public int maxDistance(int[] colors) {
+        int n = colors.length;
+
+        int max = 0;
+
+        // Compare with first house
+        for (int i = n - 1; i >= 0; i--) {
+            if (colors[i] != colors[0]) {
+                max = Math.max(max, i);
+                break;
+            }
+        }
+
+        // Compare with last house
+        for (int i = 0; i < n; i++) {
+            if (colors[i] != colors[n - 1]) {
+                max = Math.max(max, n - 1 - i);
+                break;
+            }
+        }
+
+        return max;
+    }
 }
