278ea2f43125d49939136bd4e9af39694c54fd9f
joaoscioli <joaoscioli@outlook.com>
2026-04-22 22:19:22 -0300

    feat(daily question) complete solution class to project SumOfDistances

--- src/daily_question/sumOfDistances/Solution.java
+++ src/daily_question/sumOfDistances/Solution.java
@@ -1,4 +1,42 @@
 package daily_question.sumOfDistances;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public long[] distance(int[] nums) {
+        int n = nums.length;
+        long[] result = new long[n];
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+
+        for (List<Integer> indices : map.values()) {
+            int size = indices.size();
+
+            long[] prefix = new long[size];
+            prefix[0] = indices.get(0);
+
+            for (int i = 1; i < size; i++) {
+                prefix[i] = prefix[i - 1] + indices.get(i);
+            }
+
+            for (int i = 0; i < size; i++) {
+                int idx = indices.get(i);
+
+                long left = (long) i * idx - (i > 0 ? prefix[i - 1] : 0);
+
+                long right = (prefix[size - 1] - prefix[i]) - (long) (size - i - 1) * idx;
+
+                result[idx] = left + right;
+            }
+        }
+
+        return result;
+    }
 }
