b1f49d37c3bbcf9e0cfd2740287c3afdbdaf1019
joaoscioli <joaoscioli@outlook.com>
2026-04-23 21:48:45 -0300

    feat(daily question) complete Solution class to project FurthestPointFromOrigin

--- src/daily_question/furthestPointFromOrigin/Solution.java
+++ src/daily_question/furthestPointFromOrigin/Solution.java
@@ -1,4 +1,18 @@
 package daily_question.furthestPointFromOrigin;
 
-public class Solution {
+class Solution {
+    public int furthestDistanceFromOrigin(String moves) {
+        int L = 0, R = 0, blank = 0;
+
+        for (char c : moves.toCharArray()) {
+            if (c == 'L') L++;
+            else if (c == 'R') R++;
+            else blank++;
+        }
+
+        int right = R + blank - L;
+        int left = L + blank - R;
+
+        return Math.max(Math.abs(right), Math.abs(left));
+    }
 }
