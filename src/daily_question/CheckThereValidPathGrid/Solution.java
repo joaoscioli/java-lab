13c740b3d926719ce3633c315bc29e4a84faa156
joaoscioli <joaoscioli@outlook.com>
2026-04-26 22:37:54 -0300

    feat(daily question) complete Solution to project CheckThereValidPathGrid

--- src/daily_question/CheckThereValidPathGrid/Solution.java
+++ src/daily_question/CheckThereValidPathGrid/Solution.java
@@ -1,4 +1,53 @@
 package daily_question.CheckThereValidPathGrid;
 
-public class Solution {
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+class Solution {
+    public boolean hasValidPath(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+
+        Map<Integer, int[][]> map = new HashMap<>();
+        map.put(1, new int[][]{{0,-1},{0,1}});
+        map.put(2, new int[][]{{-1,0},{1,0}});
+        map.put(3, new int[][]{{0,-1},{1,0}});
+        map.put(4, new int[][]{{0,1},{1,0}});
+        map.put(5, new int[][]{{0,-1},{-1,0}});
+        map.put(6, new int[][]{{0,1},{-1,0}});
+
+        boolean[][] visited = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.offer(new int[]{0, 0});
+        visited[0][0] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int r = cur[0], c = cur[1];
+
+            if (r == m - 1 && c == n - 1) return true;
+
+            for (int[] d : map.get(grid[r][c])) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr < 0 || nc < 0 || nr >= m || nc >= n || visited[nr][nc])
+                    continue;
+
+                for (int[] back : map.get(grid[nr][nc])) {
+                    if (nr + back[0] == r && nc + back[1] == c) {
+                        visited[nr][nc] = true;
+                        queue.offer(new int[]{nr, nc});
+                        break;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
 }
