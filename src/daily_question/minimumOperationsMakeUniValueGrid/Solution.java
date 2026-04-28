d112d3602af6069b94fe411dd477021e11729526
joaoscioli <joaoscioli@outlook.com>
2026-04-27 22:15:38 -0300

    feat(daily question) complete Solution to project minimumOperationsMakeUniValueGrid

--- src/daily_question/minimumOperationsMakeUniValueGrid/Solution.java
+++ src/daily_question/minimumOperationsMakeUniValueGrid/Solution.java
@@ -1,4 +1,36 @@
 package daily_question.minimumOperationsMakeUniValueGrid;
 
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public int minOperations(int[][] grid, int x) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int total = m * n;
+
+        int[] flat = new int[total];
+        int idx = 0;
+        for (int[] row : grid) {
+            for (int val : row) {
+                flat[idx++] = val;
+            }
+        }
+
+        int remainder = flat[0] % x;
+        for (int val : flat) {
+            if (val % x != remainder) {
+                return -1;
+            }
+        }
+
+        Arrays.sort(flat);
+        int median = flat[total / 2];
+
+        int operations = 0;
+        for (int val : flat) {
+            operations += Math.abs(val - median) / x;
+        }
+
+        return operations;
+    }
 }
