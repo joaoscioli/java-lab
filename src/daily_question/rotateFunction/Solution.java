e95e389d59d078d7a3b3d20cdf687cb998bd943d
joaoscioli <joaoscioli@outlook.com>
2026-05-01 19:46:35 -0300

    feat(daily question) complete solution to project rotateFunction

--- src/daily_question/rotateFunction/Solution.java
+++ src/daily_question/rotateFunction/Solution.java
@@ -1,4 +1,27 @@
 package daily_question.rotateFunction;
 
 public class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+
+        int sum = 0;
+        int f0 = 0;
+
+        // Compute sum and F(0)
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            f0 += i * nums[i];
+        }
+
+        int max = f0;
+        int current = f0;
+
+        // Compute F(k) using recurrence
+        for (int k = 1; k < n; k++) {
+            current = current + sum - n * nums[n - k];
+            max = Math.max(max, current);
+        }
+
+        return max;
+    }
 }
