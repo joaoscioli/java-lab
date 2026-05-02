d7e0e72443d7c205f4c545222e11ad2709dd92fe
joaoscioli <joaoscioli@outlook.com>
2026-05-02 13:40:53 -0300

    feat(daily question) complete solution to project rotateDigits

--- src/daily_question/RotatedDigits/Solution.java
+++ src/daily_question/RotatedDigits/Solution.java
@@ -1,4 +1,36 @@
 package daily_question.RotatedDigits;
 
-public class Solution {
+class Solution {
+    public int rotatedDigits(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (isGood(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isGood(int num) {
+        boolean hasChanged = false;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            // Invalid digits
+            if (digit == 3 || digit == 4 || digit == 7) {
+                return false;
+            }
+
+            // Digits that change after rotation
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
+                hasChanged = true;
+            }
+
+            num /= 10;
+        }
+
+        return hasChanged;
+    }
 }
