8f8fbd52a1b73e38492bd36d9abfe5342ea57944
joaoscioli <joaoscioli@outlook.com>
2026-05-09 22:00:11 -0300

    feat(daily question) complete Solution to project MaximumNumberJumpsReachLastIndex

--- src/daily_question/maximumNumberJumpsReachLastIndex/Solution.java
+++ src/daily_question/maximumNumberJumpsReachLastIndex/Solution.java
@@ -1,4 +1,28 @@
 package daily_question.maximumNumberJumpsReachLastIndex;
 
-public class Solution {
+class Solution {
+    public int maximumJumps(int[] nums, int target) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = -1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] == -1) {
+                    continue;
+                }
+
+                long diff = (long) nums[i] - nums[j];
+
+                if (-target <= diff && diff <= target) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
 }
