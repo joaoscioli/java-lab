4177a55830d1c8a7dd1566cd46c708c6b6747537
joaoscioli <joaoscioli@outlook.com>
2026-05-10 21:50:25 -0300

    feat(daily question) complete Solution class to project separateDigitsArray

--- src/daily_question/separateDigitsArray/Solution.java
+++ src/daily_question/separateDigitsArray/Solution.java
@@ -1,4 +1,26 @@
 package daily_question.separateDigitsArray;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        List<Integer> digits = new ArrayList<>();
+
+        for (int num : nums) {
+            String s = String.valueOf(num);
+
+            for (char c : s.toCharArray()) {
+                digits.add(c - '0');
+            }
+        }
+
+        int[] answer = new int[digits.size()];
+
+        for (int i = 0; i < digits.size(); i++) {
+            answer[i] = digits.get(i);
+        }
+
+        return answer;
+    }
 }
