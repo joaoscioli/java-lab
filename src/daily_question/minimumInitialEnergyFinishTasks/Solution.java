b6e626e8bbb0411b64909dab31e5c9fddac9c3a6
joaoscioli <joaoscioli@outlook.com>
2026-05-11 21:26:42 -0300

    feat(daily question) complete Solution class to project MinimumInitialEnergyFinishTasks

--- src/daily_question/minimumInitialEnergyFinishTasks/Solution.java
+++ src/daily_question/minimumInitialEnergyFinishTasks/Solution.java
@@ -1,4 +1,31 @@
 package daily_question.minimumInitialEnergyFinishTasks;
 
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks, (a, b) -> {
+            int diffA = a[1] - a[0];
+            int diffB = b[1] - b[0];
+            return diffB - diffA;
+        });
+
+        int initialEnergy = 0;
+        int currentEnergy = 0;
+
+        for (int[] task : tasks) {
+            int actual = task[0];
+            int minimum = task[1];
+
+            if (currentEnergy < minimum) {
+                int needed = minimum - currentEnergy;
+                initialEnergy += needed;
+                currentEnergy += needed;
+            }
+
+            currentEnergy -= actual;
+        }
+
+        return initialEnergy;
+    }
 }
