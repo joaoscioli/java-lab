ead59e5f2a16b5f15cac95574585f35094489884
joaoscioli <joaoscioli@outlook.com>
2026-05-12 21:53:31 -0300

    feat(daily question) complete Solution class to project MinimumMovesMakeArrayComplementary

--- src/daily_question/MinimumMovesMakeArrayComplementary/Solution.java
+++ src/daily_question/MinimumMovesMakeArrayComplementary/Solution.java
@@ -1,4 +1,36 @@
 package daily_question.MinimumMovesMakeArrayComplementary;
 
-public class Solution {
+class Solution {
+    public int minMoves(int[] nums, int limit) {
+        int n = nums.length;
+        int[] diff = new int[2 * limit + 2];
+
+        for (int i = 0; i < n / 2; i++) {
+            int a = nums[i];
+            int b = nums[n - 1 - i];
+
+            int min = Math.min(a, b);
+            int max = Math.max(a, b);
+            int sum = a + b;
+
+            diff[2] += 2;
+            diff[2 * limit + 1] -= 2;
+
+            diff[min + 1] -= 1;
+            diff[max + limit + 1] += 1;
+
+            diff[sum] -= 1;
+            diff[sum + 1] += 1;
+        }
+
+        int answer = Integer.MAX_VALUE;
+        int moves = 0;
+
+        for (int target = 2; target <= 2 * limit; target++) {
+            moves += diff[target];
+            answer = Math.min(answer, moves);
+        }
+
+        return answer;
+    }
 }
