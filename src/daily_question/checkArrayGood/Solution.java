8956770db1ddb7ea47c0bda0177235c800fbfb9a
joaoscioli <joaoscioli@outlook.com>
2026-05-13 22:16:51 -0300

    feat(daily question) complete Solution class to project CheckArrayGood

--- src/daily_question/checkArrayGood/Solution.java
+++ src/daily_question/checkArrayGood/Solution.java
@@ -1,4 +1,23 @@
 package daily_question.checkArrayGood;
 
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public boolean isGood(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums[nums.length - 1];
+
+        if (nums.length != n + 1) {
+            return false;
+        }
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != i + 1) {
+                return false;
+            }
+        }
+
+        return nums[nums.length - 1] == n;
+    }
 }
