330e76244a9e8f4b41b73564079cd3b811738d4f
joaoscioli <joaoscioli@outlook.com>
2026-05-15 22:32:08 -0300

    feat(daily question) complete Solution class to project FindMinimumRotatedSortedArrayII

--- src/daily_question/FindMinimumRotatedSortedArrayII/Solution.java
+++ src/daily_question/FindMinimumRotatedSortedArrayII/Solution.java
@@ -1,4 +1,22 @@
 package daily_question.FindMinimumRotatedSortedArrayII;
 
-public class Solution {
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+
+        return nums[left];
+    }
 }
