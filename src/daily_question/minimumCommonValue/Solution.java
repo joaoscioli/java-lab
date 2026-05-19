3f872652782854d457fdbcdc4a47bdf6ff883ab3
joaoscioli <joaoscioli@outlook.com>
2026-05-18 21:55:45 -0300

    feat(daily question) complete Solution class to project MinimumCommonValue

--- src/daily_question/minimumCommonValue/Solution.java
+++ src/daily_question/minimumCommonValue/Solution.java
@@ -1,4 +1,22 @@
 package daily_question.minimumCommonValue;
 
-public class Solution {
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            }
+
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return -1;
+    }
 }
