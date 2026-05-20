feeee26edf6c31570965cc5713eee87143d749c3
joaoscioli <joaoscioli@outlook.com>
2026-05-19 22:17:58 -0300

    feat(daily question) complete Solution class to project FindPrefixCommonArrayTwoArrays

--- src/daily_question/findPrefixCommonArrayTwoArrays/Solution.java
+++ src/daily_question/findPrefixCommonArrayTwoArrays/Solution.java
@@ -1,4 +1,29 @@
 package daily_question.findPrefixCommonArrayTwoArrays;
 
-public class Solution {
+class Solution {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n = A.length;
+        int[] result = new int[n];
+        int[] frequency = new int[n + 1];
+
+        int common = 0;
+
+        for (int i = 0; i < n; i++) {
+            frequency[A[i]]++;
+
+            if (frequency[A[i]] == 2) {
+                common++;
+            }
+
+            frequency[B[i]]++;
+
+            if (frequency[B[i]] == 2) {
+                common++;
+            }
+
+            result[i] = common;
+        }
+
+        return result;
+    }
 }
