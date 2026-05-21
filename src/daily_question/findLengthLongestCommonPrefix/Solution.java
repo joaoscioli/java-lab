aef2c221d7703234051e3f78bf3fa5c77acc9748
joaoscioli <joaoscioli@outlook.com>
2026-05-20 23:04:18 -0300

    feat(daily question) complete Solution class to project FindLengthLongestCommonPrefix

--- src/daily_question/findLengthLongestCommonPrefix/Solution.java
+++ src/daily_question/findLengthLongestCommonPrefix/Solution.java
@@ -1,4 +1,44 @@
 package daily_question.findLengthLongestCommonPrefix;
 
-public class Solution {
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        Set<Integer> prefixes = new HashSet<>();
+
+        for (int number : arr1) {
+            while (number > 0) {
+                prefixes.add(number);
+                number /= 10;
+            }
+        }
+
+        int longest = 0;
+
+        for (int number : arr2) {
+            while (number > 0) {
+                if (prefixes.contains(number)) {
+                    longest = Math.max(longest, countDigits(number));
+                    break;
+                }
+
+                number /= 10;
+            }
+        }
+
+        return longest;
+    }
+
+
+    private int countDigits(int number) {
+        int digits = 0;
+
+        while (number > 0) {
+            digits++;
+            number /= 10;
+        }
+
+        return digits;
+    }
 }
