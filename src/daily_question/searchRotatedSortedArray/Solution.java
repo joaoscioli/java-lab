b028844a4f649f99a646ff275c8da89b19911368
joaoscioli <joaoscioli@outlook.com>
2026-05-21 21:48:51 -0300

    feat(daily question) complete Solution to project SearchRotatedSortedArray

--- src/daily_question/searchRotatedSortedArray/Solution.java
+++ src/daily_question/searchRotatedSortedArray/Solution.java
@@ -1,4 +1,35 @@
 package daily_question.searchRotatedSortedArray;
 
-public class Solution {
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
+            }
+
+            // Left half is sorted
+            if (nums[left] <= nums[middle]) {
+                if (nums[left] <= target && target < nums[middle]) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if (nums[middle] < target && target <= nums[right]) {
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
 }
