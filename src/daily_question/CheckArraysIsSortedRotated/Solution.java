b213801a30f9d50dfbb5a4e1fa0019745280abd5
joaoscioli <joaoscioli@outlook.com>
2026-05-22 21:44:35 -0300

    feat(daily question) complete Solution class to project CheckArrayIsSortedRotated

--- src/daily_question/CheckArraysIsSortedRotated/Solution.java
+++ src/daily_question/CheckArraysIsSortedRotated/Solution.java
@@ -1,4 +1,20 @@
 package daily_question.CheckArraysIsSortedRotated;
 
-public class Solution {
+class Solution {
+    public boolean check(int[] nums) {
+        int drops = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                drops++;
+            }
+
+            if (drops > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
