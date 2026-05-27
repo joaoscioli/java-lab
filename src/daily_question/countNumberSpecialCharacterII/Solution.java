f75b64c7ffb5f11b4591e789aa811c4ab7cc699e
joaoscioli <joaoscioli@outlook.com>
2026-05-26 22:45:34 -0300

    feat(daily question) complete Solution class to project CountNumberSpecialCharactersII

--- src/daily_question/countNumberSpecialCharacterII/Solution.java
+++ src/daily_question/countNumberSpecialCharacterII/Solution.java
@@ -1,4 +1,35 @@
 package daily_question.countNumberSpecialCharacterII;
 
-public class Solution {
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int[] lastLower = new int[26];
+        int[] firstUpper = new int[26];
+
+        for (int i = 0; i < 26; i++) {
+            lastLower[i] = -1;
+            firstUpper[i] = word.length();
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            if (Character.isLowerCase(c)) {
+                lastLower[c - 'a'] = i;
+            } else {
+                int index = c - 'A';
+                firstUpper[index] = Math.min(firstUpper[index], i);
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (lastLower[i] != -1 && firstUpper[i] != word.length()
+                    && lastLower[i] < firstUpper[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
